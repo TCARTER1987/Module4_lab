0b81505313b67e5b0927dcec0d06faf241aea16c
timothy carter <tim_d87@hotmail.com>
2025-08-06 12:57:58 -0500

    Add project files.

---
+++ Module4_lab/Character.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+using namespace std;
+
+class Character {
+protected:
+    string name;
+    int level;
+    int health;
+
+public:
+    Character(const string& name, int level, int health);
+    virtual void attack() = 0; // pure virtual function
+    virtual ~Character() = default;
+};
+
+#endif
